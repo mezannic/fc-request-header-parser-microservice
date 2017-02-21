97e1e01153ea5bdd5d9cbb97dc32e8515f0aa1ad
Mathieu Ezannic <mezannic@gmail.com>
2017-02-21 06:43:49 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+var useragent = require('express-useragent')
+
+app.use(useragent.express())
+
+app.get('*', function (req, res) {
+    res.json({
+        ipaddress: req.ip,
+        language: req.get('Accept-Language'),
+        software: req.useragent.os
+    })
+})
+
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Listening on port ' + process.env.PORT)
+})
